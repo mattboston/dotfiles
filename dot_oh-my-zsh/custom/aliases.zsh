f0fdc6dd52d64bac249a4ff8df250f666b9d35c6
Matt Shields <matt@shields.tv>
2023-11-15 23:42:49 -0500

    Updates

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,8 +1,17 @@
 # Aliases
 
-alias dir='ls -lasF'
+# alias dir='ls -lasF'
+alias dir='colorls -la'
 alias k="kubectl"
 alias h="helm"
 alias tf="terraform"
 alias a="ansible"
 alias ap="ansible-playbook"
+alias cm="chezmoi"
+
+alias wgetp='wget -P ~/Downloads/'
+
+# remove % symbol from curl answer in zsh (-w) as well as other odd output (-s):
+alias curl="curl -s -w '\n'"
+
+alias ipconfig="curl ifconfig.co/json"

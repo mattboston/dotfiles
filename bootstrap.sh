54c59f62b1886ae9ebeb7c40ae11268240f0103f
Matt Shields <matt@shields.tv>
2024-10-05 19:16:22 -0400

    Updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -42,13 +42,13 @@ mkdir -p ${BIN_DIR} ${CODE_DIR}
 # ###########################################################
 # install homebrew (CLI Packages)
 # ###########################################################
-running "checking homebrew..."
+echo "checking homebrew..."
 brew_bin=$(which brew) 2>&1 > /dev/null
 if [[ $? != 0 ]]; then
-  action "installing homebrew"
+  echo "installing homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   if [[ $? != 0 ]]; then
-    error "unable to install homebrew, script $0 abort!"
+    echo "unable to install homebrew, script $0 abort!"
     exit 2
   fi
   (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$(whoami)/.zprofile
@@ -56,17 +56,17 @@ if [[ $? != 0 ]]; then
   brew analytics off
 else
   ok
-  bot "Homebrew"
+  echo "Homebrew"
   read -r -p "run brew update && upgrade? [y|N] " response
   if [[ $response =~ (y|yes|Y) ]]; then
-    action "updating homebrew..."
+    echo "updating homebrew..."
     brew update
-    ok "homebrew updated"
-    action "upgrading brew packages..."
+    echo "homebrew updated"
+    echo "upgrading brew packages..."
     brew upgrade
-    ok "brews upgraded"
+    echo "brews upgraded"
   else
-    ok "skipped brew package upgrades."
+    echo "skipped brew package upgrades."
   fi
 fi
 
